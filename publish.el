042ed3ab0e2ce525146efe69c346de1ad8769de4
Arttu Siipola <arttu.siipola@helsinki.fi>
2025-08-16 15:31:50 +0300

    second init

--- publish.el
+++ publish.el
@@ -29,6 +29,7 @@
              :base-directory "./"
              :publishing-directory "./public/"
              :publishing-function 'org-html-publish-to-html
+             :exclude "README\\.org\\|build\\.sh\\|publish\\.el\\|\\.packages\\|public/"
              :with-author nil           ; Don't include author name
              :with-creator t            ; Include Emacs and Org versions
              :with-toc nil              ; Don't include a table of contents
@@ -37,13 +38,17 @@
              :html-validation-link nil  ; Don't include validation link
              :html-head-include-scripts nil ; Don't include scripts
              :html-head-include-default-style nil ; Don't include default style
+             :auto-sitemap t            ; Generate sitemap
+             :sitemap-filename "sitemap.org"
+             :sitemap-title "Site Map"
              )
        (list "org-site:static"
              :base-directory "./"
-             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf"
+             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|txt"
              :publishing-directory "./public/"
              :recursive t
              :publishing-function 'org-publish-attachment
+             :exclude "public/"
              )
        (list "org-site" :components '("org-site:main" "org-site:static"))
        ))
